2c4ca8b384732306ed576c6e3998145f3e793ffa
muratmlz <muratmlz091@gmail.com>
2024-10-09 13:42:43 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,37 +1,27 @@
-self.addEventListener('install', event => {
-    console.log('Service Worker εγκαταστάθηκε');
-});
-
-self.addEventListener('fetch', event => {
-    console.log('Αιτήματα για:', event.request.url);
-});
-
-if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('./service-worker.js')
-    .then(() => console.log('Service Worker Registered'));
-}
-
 self.addEventListener('install',
 event => {
+    console.log('Service Worker εγκαταστάθηκε');
+
     event.waitUntil(
 
-        caches.open('reminder-cache').then(cache =>{
+        caches.open('shopping-list-cache').then(cache => {
             return cache.addAll([
-                './index.html',
-                './styles.css',
-                './script.js',
-                './manifest.json',
-                './icons/icon-192x192.png',
-                './icons/icon-512x512.png'
+                '/',
+                '/index.html',
+                '/style.css',
+                '/script.js',
+                '/manifest.json',
+                '/icons/icon-192x192.png',
+                '/icons/icon-512x512.png'
             ]);
         })
     );
 });
 
 self.addEventListener('fetch', event => {
-    event.respondWith(
-        caches.match(event.request)
-        .then(response => {
+    event.responWith(
+
+        caches.match(event.request).then(response => {
             return response ||
             fetch(event.request);
         })
